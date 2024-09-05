57e59be1341f417d7fc7a33965b8cfe131949823
Humberto Islas <islas333@gmail.com>
2024-09-04 20:53:03 -0600

    Rust sqlite

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,199 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use rusqlite::{params, Connection, Result as SqlResult};
+use tauri::command;
+use tauri::api::path::desktop_dir;
+use std::path::PathBuf;
+use serde::Serialize;
+
+#[derive(Debug, serde::Serialize)]
+struct Snippet {
+    id: i32,
+    name: String,
+    code: Option<String>, // Usar Option para manejar posibles valores nulos en la base de datos
+}
+
+#[derive(Debug, Serialize)]
+struct Patient {
+    id: i32,
+    name: String,
+    age: i32,
+}
+
+fn get_db_path() -> Result<PathBuf, String> {
+    let desktop_path = desktop_dir().ok_or("Failed to get desktop directory")?;
+    let db_path = desktop_path.join("tauri").join("my_database.db");
+    
+    println!("Database path: {:?}", db_path);
+
+    Ok(db_path)
+}
+
+fn init_db() -> SqlResult<()> {
+    
+    let db_path = get_db_path().unwrap();
+    let conn: Connection = Connection::open(db_path)?;
+
+    // Crea una tabla de ejemplo
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS snippets (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            code TEXT
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS patients (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            age INTEGER
+        )",
+        [],
+    )?;
+
+    println!("Database initialized successfully.");
+    Ok(())
+}
+
+#[command]
+fn insert_user(name: &str, code: &str) -> Result<(), String> {
+    println!("Attempting to insert user: name = {}, code = {}", name, code);
+
+    let db_path = get_db_path().unwrap();
+
+    // Manejo del error con un tipo de retorno compatible con Tauri
+    let conn = Connection::open(db_path).map_err(|e| {
+        let err_msg = format!("Failed to open the database: {}", e);
+        eprintln!("{}", err_msg);
+        err_msg
+    })?;
+
+    conn.execute(
+        "INSERT INTO snippets (name, code) VALUES (?1, ?2)",
+        params![name, code],
+    )
+    .map_err(|e| {
+        let err_msg = format!("Failed to execute the insert statement: {}", e);
+        eprintln!("{}", err_msg);
+        err_msg
+    })?;
+
+    println!("User inserted successfully.");
+    Ok(())
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_snippets() -> Result<Vec<Snippet>, String> {
+
+    let db_path = get_db_path().unwrap();
+
+    // Abre la conexión a la base de datos
+    let conn = Connection::open(db_path).map_err(|e| {
+        let err_msg = format!("Failed to open the database: {}", e);
+        eprintln!("{}", err_msg);
+        err_msg
+    })?;
+
+    // Define la consulta SQL
+    let query = "SELECT id, name, code FROM snippets";
+    
+    // Prepara la consulta
+    let mut stmt = conn.prepare(query).map_err(|e| {
+        let err_msg = format!("Failed to prepare the query: {}", e);
+        eprintln!("{}", err_msg);
+        err_msg
+    })?;
+
+    // Ejecuta la consulta y mapea los resultados a un vector de `Snippet`
+    let snippet_iter = stmt
+        .query_map([], |row| {
+            Ok(Snippet {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                code: row.get(2)?, // Ajustado para manejar valores `NULL` en la base de datos
+            })
+        })
+        .map_err(|e| {
+            let err_msg = format!("Failed to execute the query: {}", e);
+            eprintln!("{}", err_msg);
+            err_msg
+        })?;
+
+    // Recoge los resultados
+    let mut snippets = Vec::new();
+    for snippet in snippet_iter {
+        match snippet {
+            Ok(snippet) => snippets.push(snippet),
+            Err(e) => eprintln!("Error retrieving snippet: {}", e),
+        }
+    }
+
+    Ok(snippets)
+}
+
+#[tauri::command]
+fn get_v2() -> Result<Vec<Snippet>, String> {
+    let conn = Connection::open("my_database.db").expect("Error en la conexión a la base de datos");
+
+    let mut _statement = conn.prepare("SELECT id, name, code FROM snippets").expect("Error en la preparación de la consulta");
+
+    let iterador_snippets = _statement.query_map([], |row| {
+        Ok(Snippet {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            code: row.get(2)?,
+        })
+    }).expect("Error en la ejecución de la consulta");
+
+    let mut snippets = Vec::new();
+    for snippet in iterador_snippets {
+        match snippet {
+            Ok(snippet) => snippets.push(snippet),
+            Err(e) => eprintln!("Error recuperando el snippet: {}", e),
+        }
+    }
+
+    Ok(snippets)
+}
+
+#[tauri::command]
+fn get_patient(id: i32) -> Result<Patient, String> {
+
+    let db_path = get_db_path().unwrap();
+    let conn: Connection = Connection::open(db_path).expect("Error en la preparación de la consulta");
+   
+    let patient = conn.query_row(
+        "SELECT id, name, age FROM patients WHERE id = ?1",
+        params![id],
+        |row| {
+            Ok(Patient {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                age: row.get(2)?,
+            })
+        },
+    ).map_err(|e| e.to_string())?;
+
+    Ok(patient)
+}
+
 fn main() {
+    if let Err(e) = init_db() {
+        eprintln!("Error initializing database: {}", e);
+    }
+
+    get_patient(1);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, insert_user, get_snippets, get_v2, get_patient])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/SnippetList.tsx
+++ src/components/SnippetList.tsx
@@ -1,14 +1,52 @@
 import { readDir } from "@tauri-apps/api/fs"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { desktopDir } from "@tauri-apps/api/path"
 import { useSnippetStore } from '../store/SnippetsStore'
 import SnippetItem from './SnippetItem';
+import { invoke } from '@tauri-apps/api/tauri'
 
+
+interface Patient {
+  id: number;
+  name: string;
+  age: number;
+}
 
 function SnippetList() {
 
   const setSnippetsNames = useSnippetStore((state) => state.setSnippetsNames)
   const snippetsNames = useSnippetStore((state) => state.snippetsNames)
+  const [snippetName, setSnippetName] = useState<string>('')
+
+  async function addUser(name: string, data: string) {
+    try {
+      await invoke('insert_user', { name: name, code: data });
+      alert('User added successfully!');
+    } catch (error) {
+      console.error('Error adding user:', error);
+    }
+  }
+
+  const handleGetAll = async () => {
+    try {
+      const snippets: Array<{ name: string }> = await invoke('get_snippets');
+      console.log('snippets:', snippets);
+
+      const getv2: Array<{ name: string }> = await invoke('get_v2');
+      console.log('getv2:', getv2);
+
+      const patient = await invoke<Patient>('get_patient', { id: 2 });
+      console.log(`Edad del paciente: ${patient.name}`); 
+
+      setSnippetName(snippets[0].name)
+    } catch (error) {
+      console.error('Error getting snippets:', error);
+    }
+  }
+
+  const handleInsert = async () => {
+    addUser("tito", "developer")
+  }
 
   useEffect(() => {
     async function loadFiles() {
@@ -24,10 +62,21 @@ function SnippetList() {
   return (
     <ul>
       {snippetsNames
-      .filter(name => name !== '.DS_Store')
-      .map((name) => (
-        <SnippetItem key={name} file={name} />
-      ))}
+        .filter(name => name !== '.DS_Store')
+        .map((name) => (
+          <SnippetItem key={name} file={name} />
+        ))}
+      <button className="bg-zinc-800 w-full p-4"
+        onClick={() => {
+          handleInsert()
+        }}
+      >Add data Snippet</button>
+      <button className="bg-zinc-800 w-full p-4"
+        onClick={() => {
+          handleGetAll()
+        }}
+      >Read Snippet</button>
+      <span>{snippetName}</span>
     </ul>
   )
 }
